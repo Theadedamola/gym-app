66fd7fc2ca686ffdf261e30819644f6e99a98b25
Theadedamola <adedamolajose@gmail.com>
2024-07-13 22:23:17 +0100

    navbar types and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Navbar from '@/scenes/navbar'
+import { useState } from 'react'
+import { SelectedPage } from '@/share/typs'
+
 function App() {
-  return <div className="app">app</div>
+
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+
+    return <div className="app bg-gray-20 ">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+    </div>
 }
 
 export default App
